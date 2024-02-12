c84154c3e6b6135fd4919cc4443925e0dbadb7a5
Eyob <eyobhg3@gmail.com>
2024-02-12 16:17:00 +0300

    Create views for mark as done and delete task methods

--- To-Do-App/Project-To-Do/todo_project/to_do_app/views.py
+++ To-Do-App/Project-To-Do/todo_project/to_do_app/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Task
+from .forms import TaskForm
 
-# Create your views here.
+
+def index(request):
+    form = TaskForm()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        
+    tasks = Task.objects.all()
+    context = {'tasks':tasks, 'form':form}
+    return render(request, 'to_do_app/index.html', context)
+
+def mark_as_done(request, task_id):
+    task = Task.objects.get(pk=task_id)
+    task.status = True if task.status==False else False
+    task.save()
+    return redirect('home')
+
+
+def delete_task(request, task_id):
+    task = Task.object.get(pk=task_id)
+    task.delete()
+    return redirect('home')
+
+def edit_task(request, task_id):
+    pass
+def view_task(request, task_id):
+    pass
