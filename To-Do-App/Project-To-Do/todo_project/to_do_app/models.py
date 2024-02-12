7d953f9cfd37186e7c028245edeaffddcbd8cf4a
Eyob <eyobhg3@gmail.com>
2024-02-12 14:45:54 +0300

    create the task model

--- To-Do-App/Project-To-Do/todo_project/to_do_app/models.py
+++ To-Do-App/Project-To-Do/todo_project/to_do_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    desc = models.CharField(max_length=600)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
+    due_date = models.DateTimeField(editable=True)
+    status = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.title
