434257d60177c091732587bcbb572aa545ff100f
Eyob <eyobhg3@gmail.com>
2024-02-12 10:15:36 +0300

    Begin new refresher projects

--- Storefront/playground/urls.py
+++ Storefront/playground/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 # from .views import sayHi
 from . import views
 
-
+#URLconf  url configuration
 urlpatterns = [
     path('hi/', views.sayHi)
 ]
--- Storefront/playground/views.py
+++ Storefront/playground/views.py
@@ -6,5 +6,10 @@ from django.http import HttpResponse
 #req->res
 
 #Say hi
+# def sayHi(request):
+#     return HttpResponse("Hi")
+
+#say hi from template
 def sayHi(request):
-    return HttpResponse("Hi")
+    # return render(request, 'hi.html', {'name': 'Mr. X'})
+    return render(request, 'hi.html')
