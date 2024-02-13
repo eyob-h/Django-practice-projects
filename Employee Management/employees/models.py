9d6b16cf7210884ec525e74ba3a3352f76ff53d6
Eyob <eyobhg3@gmail.com>
2024-02-13 22:20:44 +0300

    make the employees model and migrate it

--- Employee Management/employees/models.py
+++ Employee Management/employees/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    role = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='images')
+    email = models.CharField(max_length=60, unique=True)
+    phone = models.CharField(max_length=15)
+    address = models.CharField(max_length=100)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
