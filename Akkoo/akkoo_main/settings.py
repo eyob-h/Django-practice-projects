5764d48c2bb3bc97237576cb1fe005a868ab5902
Eyob <eyobhg3@gmail.com>
2024-02-16 14:23:38 +0300

    configure super user, add gitignore, and setup home page

--- Akkoo/akkoo_main/settings.py
+++ Akkoo/akkoo_main/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'akkoo_main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
