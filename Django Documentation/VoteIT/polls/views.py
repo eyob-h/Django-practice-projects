a13333e856e198a0c3500f42991ff0cd8c458f6b
Eyob <eyobhg3@gmail.com>
2024-02-27 13:38:34 +0300

    Add completed code from the documentation tutorial part 3.

--- Django Documentation/VoteIT/polls/views.py
+++ Django Documentation/VoteIT/polls/views.py
@@ -1,6 +1,38 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from .models import Question, Choice
+from django.http import Http404
 
-# Create your views here.
+#All view
+def index(request):
+    latest_question_list = Question.objects.order_by("-publication_date")[:5]
+    context = {
+        'latest_question_list' : latest_question_list
+    }
 
-def index (request):
-    return HttpResponse("Hi from the polls directory")
+    return render(request, "polls/index.html" ,context)
+
+
+#Detail View
+# def detail(request, question_id):
+#     return HttpResponse("You're looking at question %s." % question_id)
+
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist.")
+#     return render(request, 'polls/detail.html', {"question":question})
+
+#Using a shortcut for the try except method
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html',{'question':question})
+
+#Results view
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
--- Django Documentation/VoteIT/voteit/settings.py
+++ Django Documentation/VoteIT/voteit/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 'polls',
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Addis_Ababa'
 
 USE_I18N = True
 
