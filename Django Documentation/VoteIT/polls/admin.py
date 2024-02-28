030b7e2fb4e3ab11e6e944c9f31bfc64e29e7e47
Eyob <eyobhg3@gmail.com>
2024-02-28 21:06:44 +0300

    Add admin site customization

--- Django Documentation/VoteIT/polls/admin.py
+++ Django Documentation/VoteIT/polls/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
-from .models import Question, Choice
-# Register your models here.
 
-admin.site.register(Question)
-admin.site.register(Choice)
+from .models import Question, Choice
+
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Question Information", {"fields": ["question_text"]}),
+        ("Date Information", {"fields": ["publication_date"]}),
+    ]
+
+    list_display = ["question_text", "publication_date", "was_published_recently"]
+
+    inlines = [ChoiceInline]
+    list_filter = ["publication_date"]
+    search_fields = ['question_text']
+admin.site.register(Question, QuestionAdmin)
--- Django Documentation/VoteIT/polls/models.py
+++ Django Documentation/VoteIT/polls/models.py
@@ -1,5 +1,6 @@
 import datetime
 from django.db import models
+from django.contrib import admin
 from django.utils import timezone
 
 # Create your models here.
@@ -10,9 +11,18 @@ class Question(models.Model):
     question_text = models.TextField(max_length=200)
     publication_date = models.DateTimeField("date published")
 
+    @admin.display(
+        boolean=True,
+        ordering="publication_date",
+        description="Published Recently?",
+    )
     def was_published_recently(self):
-        return self.publication_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.publication_date <= now
 
+    # def was_published_recently(self):
+    #     return self.publication_date >= timezone.now() - datetime.timedelta(days=1)
+    
     def __str__(self):
         return self.question_text
 
