0f8f521fa153ca89c57efe5304ebb06e7774e96d
Eyob <eyobhg3@gmail.com>
2024-02-29 16:18:54 +0300

    create custom user models and customize admin site

---
+++ Akkoo/accounts/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import UserAdmin
+# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+    ordering = ('-date_joined',) #don't forget the comma
+    list_display = ['first_name','last_name','email', 'role', 'is_active']  
+
+# class UserAdmin(admin.ModelAdmin):
+#     fields = ['first_name','last_name','email', 'role']
+
+
+admin.site.register(User, CustomUserAdmin)
