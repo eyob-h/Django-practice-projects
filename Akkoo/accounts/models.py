b1691308e2ffee13d0594f49970bd392059b75f1
Eyob <eyobhg3@gmail.com>
2024-02-29 19:31:20 +0300

    create UserProfile model.

--- Akkoo/accounts/models.py
+++ Akkoo/accounts/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-
+from django.db.models.fields.related import  OneToOneField
+# from django.db.models.fields.related import ForeignKey
+# from django.contrib.gis.db import models as gismodels
+# from django.contrib.gis.geos import Point
 
 class UserManager(BaseUserManager):
     def create_user(self, first_name, last_name, username, email, password=None):
@@ -82,3 +85,31 @@ class User(AbstractBaseUser):
             user_role = 'Customer'
         return user_role
     
+
+class UserProfile(models.Model):
+    user = OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='users/profile_pictures', blank=True, null=True)
+    cover_photo = models.ImageField(upload_to='users/cover_photos', blank=True, null=True)
+    address_line_1 = models.CharField(max_length=250, blank=True, null=True)
+    address_line_2 = models.CharField(max_length=250, blank=True, null=True)
+    country = models.CharField(max_length=15, blank=True, null=True)
+    state = models.CharField(max_length=15, blank=True, null=True)
+    city = models.CharField(max_length=15, blank=True, null=True)
+    pin_code = models.CharField(max_length=6, blank=True, null=True)
+    latitude = models.CharField(max_length=20, blank=True, null=True)
+    longitude = models.CharField(max_length=20, blank=True, null=True)
+    # location = gismodels.PointField(blank=True, null=True, srid=4326)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+  
+    def __str__(self):
+        return self.user.email
+
+
+    # def save(self, *args, **kwargs):
+    #     if self.latitude and self.longitude:
+    #         self.location = Point(float(self.longitude), float(self.latitude))
+    #         return super(UserProfile, self).save(*args, **kwargs)
+    #     return super(UserProfile, self).save(*args, **kwargs)    
+
--- Akkoo/akkoo_main/settings.py
+++ Akkoo/akkoo_main/settings.py
@@ -126,6 +126,11 @@ STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS =[
     'akkoo_main/static'
 ]
+
+#Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
