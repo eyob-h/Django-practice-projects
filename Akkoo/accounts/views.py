901a89cb0a6e5f5eeaf22330b0d206a164b9c222
Eyob <eyobhg3@gmail.com>
2024-03-08 16:07:30 +0300

    Add customer registration view and template

--- Akkoo/accounts/views.py
+++ Akkoo/accounts/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from .forms import UserForm
+from .models import User
 
 # Create your views here.
 
 def registerUser(request):
-    return HttpResponse("User Registration")
+    # return HttpResponse("User Registration")
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            # #1 Saving the password using the form
+            # password = form.cleaned_data['password']
+            # user = form.save(commit=False)
+            # user.set_password(password)
+            # user.role = user.CUSTOMER
+            # form.save()
+            # return redirect('registerUser')
+            
+            # #2 Saving the password using the create_user method we made in the User model
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+            user.role = User.CUSTOMER
+            user.save()
+    
+    else:
+        form = UserForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/registerUser.html', context)
