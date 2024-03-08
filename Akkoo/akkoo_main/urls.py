1303aab28b7f02ceb43b37973f15c6cfa010a6bb
Eyob <eyobhg3@gmail.com>
2024-03-08 14:19:10 +0300

    setup template inheritance

--- Akkoo/akkoo_main/urls.py
+++ Akkoo/akkoo_main/urls.py
@@ -19,8 +19,10 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home' ),
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
