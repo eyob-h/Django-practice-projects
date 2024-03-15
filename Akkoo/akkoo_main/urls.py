9afe933e32268672db366a18da92d26afc24bf99
Eyob <eyobhg3@gmail.com>
2024-03-15 08:54:22 +0300

    create vendor app and make its model

--- Akkoo/akkoo_main/urls.py
+++ Akkoo/akkoo_main/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home' ),
     path('accounts/', include('accounts.urls')),
+    path('vendors/', include('vendors.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
