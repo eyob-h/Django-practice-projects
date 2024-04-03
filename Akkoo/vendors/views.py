e1208ab00005c1f409836771bed891d5527db0ad
Eyob <eyobhg3@gmail.com>
2024-04-03 15:46:32 +0300

    Add the functionality to fetch food items based on category

--- Akkoo/vendors/views.py
+++ Akkoo/vendors/views.py
@@ -13,6 +13,12 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 
 from accounts.views import check_role_vendor
 
+from menu.models import Category, FoodItem
+
+#get vendor helper function
+def get_vendor(request):
+    vendor = Vendor.objects.get(user = request.user)
+    return vendor
 
 @login_required(login_url='login')
 @user_passes_test(check_role_vendor)
@@ -50,3 +56,27 @@ def vprofile(request):
         'vendor':vendor,
     }
     return render(request, 'vendors/vprofile.html', context)
+
+
+def menu_builder(request):
+    # vendor = get_vendor(request)
+    vendor = get_vendor(request)
+    categories = Category.objects.filter(vendor=vendor).order_by('created_at')
+    context = {
+        'categories': categories,
+    }
+    return render(request, 'vendors/menu_builder.html', context)
+
+
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
+def fooditems_by_category(request, pk=None):
+    vendor = get_vendor(request)
+    category = get_object_or_404(Category, pk=pk)
+    fooditems = FoodItem.objects.filter(vendor=vendor, category=category)
+    context = {
+        'fooditems': fooditems,
+        'category': category,
+    }
+    return render(request, 'vendors/fooditems_by_category.html', context)
+
